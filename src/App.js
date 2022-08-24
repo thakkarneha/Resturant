39d69f19963b15c43dd5f4b464b1d42ccbe5ca96
thakkarNeha484 <nehathakkar234@gmail.com>
2022-08-24 12:25:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Bakery from './components/Bakery';
+
+
+
+const App = () => {
+  return (
+    <div>
+     
+<Bakery/>
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ src/components/Bakery.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Menu from './Menuapi';
+import "./style.css";
+
+import Navbar from "./Navbar";
+import Menucard from './Menucard';
+
+const uniqueList = [
+  ...new Set(
+    Menu.map((curElem) => {
+      return curElem.category;
+    })
+  ),
+  "All",
+];
+
+console.log(uniqueList);
+
+const Bakery = () => {
+  const [menuData, setMenuData] = useState(Menu);
+  const [menuList, setMenuList] = useState(uniqueList);
+
+  const filterItem = (category) => {
+    if (category === "All") {
+      setMenuData(Menu);
+      return;
+    }
+
+    const updatedList = Menu.filter((curElem) => {
+      return curElem.category === category;
+    });
+
+    setMenuData(updatedList);
+  };
+
+  return (
+    <>
+      <Navbar filterItem={filterItem} menuList={menuList} />
+      <Menucard menuData={menuData} />
+    </>
+  );
+};
+
+export default Bakery
